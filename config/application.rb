f9767a4a3f4be8884e878d4ca6d9be0093b2d5c1
Michael Mitchell <michaelmitchell@gmail.com>
2020-03-13 14:10:23 -0700

    allow cors posts

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ module MimusAPI
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :options]
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
       end
     end
 
